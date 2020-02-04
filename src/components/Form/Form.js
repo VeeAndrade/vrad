c327b4c868eea835484032ef57cacf4c78c9cbf9
Veronica Andrade <veronica726@gmail.com>
2020-02-04 16:26:55 -0700

    Move updateUserInfo function to the form component as well as remove some code that was not needed, the button element was changed to the link element  so that it reroutes to the beginning

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default class Form extends Component {
   constructor() {
@@ -12,14 +14,16 @@ export default class Form extends Component {
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
   }
+   updateUserInfo = (user, purpose) => {
+    this.setState({ user })
+    this.setState({ purpose })
+  }
 
   validateUser = e => {
-    e.preventDefault();
     if(!this.state.user || !this.state.purpose || !this.state.email) {
       window.alert('You must fill out all fields')
     } else {
-      const { updateUserInfo } = this.props;
-      updateUserInfo(this.state.user, this.state.purpose);
+      this.updateUserInfo(this.state.user, this.state.purpose);
     }
   }
 
@@ -28,14 +32,14 @@ export default class Form extends Component {
       <form>
         <input
           type='text'
-          placeholder='enter username'
+          placeholder='Name'
           value={this.state.title}
           name='user'
           onChange={this.handleChange}
         />
         <input
           type='text'
-          placeholder='enter email'
+          placeholder='Email'
           name='email'
           onChange={this.handleChange}
         />
@@ -45,9 +49,9 @@ export default class Form extends Component {
           <option value='Business'>Business</option>
           <option value='Other'>Other</option>
         </select>
-        <button onClick={this.validateUser}>
-          Log In!
-        </button>
+        <Link onClick={this.validateUser} to={{pathname:'/areas', state:{user: this.state.user, purpose: this.state.purpose}}}>
+          Continue
+        </Link>
       </form>
     )
   }
