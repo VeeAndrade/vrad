303d5aab13f2d174fef0400680d83d31db27fe93
Veronica Andrade <veronica726@gmail.com>
2020-02-05 16:14:17 -0700

    Add a fetch call and get our area cards to appear.

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Greeting from '../Greeting/Greeting'
+import AreaCard from '../AreaCard/AreaCard';
 
-class Area extends React.Component {
+class Areas extends Component {
   constructor() {
     super()
+    this.state = {
+      areas: '',
+      listings: '',
+      userFavorites: ''
+    }
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/areas')
+      .then(response => response.json())
+      .then(areas => {
+        const areaPromises = areas.areas.map(area => {
+        return fetch('http://localhost:3001' + area.details)
+          .then(res => res.json())
+          .then(data => {
+            return {
+              shortName: area.area,
+              ...data
+            }
+          })
+          .catch(error => console.log(error))
+        })
+        Promise.all(areaPromises)
+        .then(areaValues => this.setState({areas: areaValues}))
+    })
+  }
+
+displayAreas = () => {
+  return this.state.areas.map(area => {
+    return <AreaCard 
+      shortName={area.shortName} 
+      name={area.area}
+      description={area.about}
+      />
+    })
+  }
+  
   render() {
-    return(
-      <div>
-        <NavLink to='/'> {this.props.location.state.user} </NavLink>
-        <Greeting user={this.props.location.state.user} purpose={this.props.location.state.purpose} />
-        <h1>Denver Areas</h1>
-        
-      </div>
-    )
+    if(!this.state.areas) {
+      return <h1>Loading...</h1>
+    } else {
+      return(
+        <main>
+          <NavLink to='/'>Signout</NavLink>
+          <Greeting user={this.props.location.state.user} purpose={this.props.location.state.purpose} />
+          <h1>Denver Areas</h1>
+          {this.displayAreas()}
+        </main>
+      )
+    }
   }
 }
 
-export default Area;
+export default Areas;
