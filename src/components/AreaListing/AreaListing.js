72533ec093360073cd893c78f8e118c1115035f7
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-05 18:23:37 -0700

    Initial commit

---
+++ src/components/AreaListing/AreaListing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AreaListing = (areaData) => {
+  // debugger
+  console.log(areaData);
+  return(
+    <article>
+      <header>name</header>
+      <button>View This Listing</button>
+    </article>
+  );
+};
+
+export default AreaListing;
