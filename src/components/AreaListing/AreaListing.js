c9c9204b9d764dca854661ff8519c67a411decef
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-07 15:50:19 -0700

    Refactor to accept necessary data

--- src/components/AreaListing/AreaListing.js
+++ src/components/AreaListing/AreaListing.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AreaListing = (id) => {
-  return(
-    <article>
+const AreaListing = ({ listings, id }) => {
+  return listings.map(listing => {
+    return (
+      <article key={listing.split('/').reverse()[0]}>
       <header>name</header>
-      <Link to={`areas/${id}` }>View This Listing</Link>
-    </article>
-  );
+      <Link to={ `areas/${id}` }>View This Listing</Link>
+      </article>
+    )
+  })
 };
 
 export default AreaListing;
