a03f125cc5c2c21dc642180e75be8f871d453786
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-09 17:20:00 -0700

    Adjust for greeting to display

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Greeting.css';
 
-const Greeting = ({ user, purpose }) => {
+const Greeting = ({ user, purpose, userFavorites }) => {
   if(!user || !purpose) {
     return ''
   } else {
     return(
       <article className='user-info'>
-      <p>
-      Hello, {user}. You've chosen {purpose} as your
-      reason for being here!
-      </p>
+        <Link to='/favorites'>{userFavorites.length}</Link>
+        <p>
+          Hello, {user}. You've chosen {purpose} as your
+          reason for being here!
+        </p>
       </article>
     )}
 
