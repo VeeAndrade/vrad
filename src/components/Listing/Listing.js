326a1bf53e5be639d4cfc48b4032eaaf4b0e0a8d
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-09 17:20:21 -0700

    Adjust for favorite functionality

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -4,7 +4,6 @@ const Listing = (props) => {
   if(!props.matched){
     return <h1>Loading...</h1>
   } else {
-    console.log(props.matched);
     let id = props.matched.listing_id.toString()
     return (
     <article>
@@ -16,47 +15,21 @@ const Listing = (props) => {
       </section>
       <ul><p>This place includes:</p>
         {props.matched.details.features.map(feature => {
-          return (<li>{feature}</li>)
+          return (<li key={feature}>{feature}</li>)
         })}
       </ul>
       <section className='description-section'>
-        <p>{props.matched.address.street}</p>
-        <p>{props.matched.address.zip}</p>
-        <p>{props.matched.details.beds}</p>
-        <p>{props.matched.details.baths}</p>
-        <p>{props.matched.details.cost_per_night}</p>
+        <p>Address: {props.matched.address.street}</p>
+        <p>Zip Code: {props.matched.address.zip}</p>
+        <p>Beds: {props.matched.details.beds}</p>
+        <p>Baths: {props.matched.details.baths}</p>
+        <p>Cost Per Night: ${props.matched.details.cost_per_night}</p>
       </section>
-      <button>Favorite</button>
+      <button onClick={() => props.addToFavorites(props.matched)}>Favorite</button>
     </article>
   )
   }
 }
-// Address
-// Number of Bedrooms
-// Number of Bathrooms
-// Cost per Night
-// All pictures of the listing
-// Unique features of the listing
 
-// {listing_id: 744, area_id: 590, name: "Upscale Modern Apartments", address: {…}, details: {…}, …}
-// listing_id: 744
-// area_id: 590
-// name: "Upscale Modern Apartments"
-// address: {street: "2345 Walnut St", zip: 80205}
-// details:
-// neighborhood_id: 5124122
-// superhost: false
-// seller_source: "91jss1"
-// beds: 2
-// baths: 2.5
-// cost_per_night: 299
-// features: (3) ["workout facility", "fireplace", "patio with grill"]
-// __proto__: Object
-// dev_id: "k6h524"
-// area: "rino"
-// db_connect: 233658
-// __proto__: Object
-
-// A button to “Favorite” the listing
 
 export default Listing;
