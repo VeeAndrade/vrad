3a0e6d3f2fd95f5e8cb999f047654dd8daea401d
Veronica Andrade <veronica726@gmail.com>
2020-02-10 19:30:26 -0700

    Write test for area listing component

---
+++ src/components/AreaListing/AreaListing.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import  { shallow } from 'enzyme';
+import AreaListing from './AreaListing';
+
+describe('AreaListing', () => {
+  it('should match the snapshot with all the data passed in correctly', () => {
+    const mockListings = [{
+      listing_id: 3,
+      area_id: 590,
+      name: "Hip RiNo Party Spot",
+      address: {
+        street: "2250 Lawrence St",
+        zip: "80205"
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 3,
+        baths: 2.5,
+        cost_per_night: 420,
+        features: [
+          "hot tub",
+          "espresso machine"
+        ],
+      },
+      dev_id: "u4gh2j",
+      area: "rino",
+      db_connect: 834470
+    }];
+    const wrapper = shallow(<AreaListing 
+    listings={mockListings}
+    id={590}
+    />)
+
+  expect(wrapper).toMatchSnapshot();
+  })
+})
