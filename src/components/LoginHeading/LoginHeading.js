81a68c82ae5a8ae9727408cd63db15631161e516
Veronica Andrade <veronica726@gmail.com>
2020-03-13 17:35:01 -0600

    Create simple component to show a conditional render on app whether the user is logged in or not

---
+++ src/components/LoginHeading/LoginHeading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginHeading.css';
+
+const LoginHeader = () => {
+  return (
+    <header className='App-header'>
+      <h1 className='App-heading'>Denver Rentals</h1>
+    </header>
+  )
+}
+
+export default LoginHeader;
