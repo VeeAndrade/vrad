31cb79f659db6e7cb15345f53e8c28cc9999703f
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-14 12:48:18 -0800

    Updates

---
+++ precache-manifest.8b63d049221960ca637aa7aba0bad5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114a03de40ef6334c22efd2a4ec89d5d",
+    "url": "/vrad/index.html"
+  },
+  {
+    "revision": "4f5c832fa60466d3cd44",
+    "url": "/vrad/static/css/main.cd04225c.chunk.css"
+  },
+  {
+    "revision": "65cf053d16a6de1ea0cd",
+    "url": "/vrad/static/js/2.760ed324.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/vrad/static/js/2.760ed324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5c832fa60466d3cd44",
+    "url": "/vrad/static/js/main.136de543.chunk.js"
+  },
+  {
+    "revision": "ca236a9d7cd818b2065d",
+    "url": "/vrad/static/js/runtime-main.0593efba.js"
+  },
+  {
+    "revision": "028aba169940e2a3961b94c72104ddf5",
+    "url": "/vrad/static/media/other-background.028aba16.jpg"
+  }
+]);
